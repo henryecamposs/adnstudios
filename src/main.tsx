eb367cd24cb5a30135480ed8db41dbe7ad3b59be
henryecamposrv <hcampos@dynamics.life>
2025-03-13 22:07:34 -0400

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
 import App from './App.tsx';
 import './index.css';
-
-createRoot(document.getElementById('root')!).render(
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+  <StrictMode>
     <App />
-);
+  </StrictMode>,
+  document.getElementById('root')
+ );
+/* createRoot(document.getElementById('root')!).render(
+  
+); */
